48863636ad24015335e506740846735ceb8d80f2
anyhowstep <anyhowstep@hotmail.com>
2019-07-23 13:49:53 -0400

    Modified ValueNextFunction.success() param

--- src/value-handler/value-next-function.ts
+++ src/value-handler/value-next-function.ts
@@ -5,7 +5,7 @@ export interface ValueNextFunction<NextLocalsT extends Locals> {
         Performs a deep merge on `res.locals` and `nextLocals`.
         Will not let you change values, only merge objects.
     */
-    readonly success : (nextLocals : NextLocalsT) => void,
+    readonly success : (nextLocals : Readonly<NextLocalsT>) => void,
     /**
         The same as calling `next(err)` in `express`
     */
